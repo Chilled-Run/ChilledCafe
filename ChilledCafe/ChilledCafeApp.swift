15cc095023a50441dca6f5f8503231a6a2141df1
Anti9uA <didlaak6000@naver.com>
2022-10-19 15:02:18 +0900

    init

---
+++ ChilledCafe/ChilledCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChilledCafeApp.swift
+//  ChilledCafe
+//
+//  Created by Kyubo Shim on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ChilledCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
