0b473a5572f1803daf11d2ded874df1ff79246e2
Terry-Koo <xpflxhfl94@naver.com>
2022-10-22 13:22:06 +0900

    [feat] #5 MainCategoryView 구현

--- ChilledCafe/ChilledCafeApp.swift
+++ ChilledCafe/ChilledCafeApp.swift
@@ -10,21 +10,23 @@ import Firebase
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
 }
 
 @main
 struct ChilledCafeApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
+    
     var body: some Scene {
         WindowGroup {
-            MainView()
+            MainCategoryView()
+                .environmentObject(FirebaseStorageManager())
         }
     }
 }
