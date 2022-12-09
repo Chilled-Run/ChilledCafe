f6d68633694ef8903f59d345e9b3ac77b1096aef
Cookie <youbellgun31@gmail.com>
2022-12-09 15:45:28 +0900

    [feat] #100 WindowGroup 복구

--- ChilledCafe/ChilledCafeApp.swift
+++ ChilledCafe/ChilledCafeApp.swift
@@ -29,63 +29,62 @@ struct ChilledCafeApp: App {
     
     var body: some Scene {
         WindowGroup {
-            CommnetView(post: constant().storySample, pawForegroundColor: Color("customGreen"), pawBackgroundColor: Color("pastelGreen"), otherFootPrintName: "catPaw")
-//            GeometryReader { geometry in
-//                if isLoading {
-//                    LaunchScreenView()
-//                        .edgesIgnoringSafeArea(.all)
-//                        .onAppear {
-//                            let dispatchGroup = DispatchGroup()
-//                            DispatchQueue.global().async(group: dispatchGroup) {
-//                                dispatchGroup.enter()
-//                                firebaseSM.getCafes(dispatchGroup: dispatchGroup)
-//                            }
-//                            dispatchGroup.notify(queue: DispatchQueue.main) {
-//                                isLoading.toggle()
-//                            }
-//                        }
-//                } else {
-//                    NavigationView {
-//                        if isFirst {
-//                            IntroView()
-//                        } else {
-//                            VStack(spacing: 0) {
-//                                switch viewRouter.currentPage {
-//                                case .home:
-//                                    MainView(firebaseSM: firebaseSM, ifFirst: true)
-//                                case .bookmarked:
-//                                    MyBookmarkView(firebaseSM: firebaseSM)
-//                                }
-//                                Spacer(minLength: 0)
-//                                ZStack {
-//                                    HStack {
-//                                        // 홈
-//                                        TabBarIcon(viewRouter: viewRouter, assignedPage: .home, width: geometry.size.width/3, height: geometry.size.height/28, iconName: "home")
-//
-//                                        // AR
-//                                        NavigationLink(destination: ARMainView()) {
-//                                            VStack {
-//                                                Image("ar")
-//                                                    .resizable()
-//                                                    .aspectRatio(contentMode: .fit)
-//                                                    .frame(width: geometry.size.width/3, height: geometry.size.height/28)
-//                                                    .foregroundColor(.gray)
-//                                            }
-//                                        }
-//
-//                                        // 북마크
-//                                        TabBarIcon(viewRouter: viewRouter, assignedPage: .bookmarked, width: geometry.size.width/3, height: geometry.size.height/28, iconName: "bookmarks")
-//                                    }
-//                                    .frame(width: geometry.size.width, height: geometry.size.height/14)
-//                                    .overlay(Divider(), alignment: .top)
-//                                }
-//                                .padding(0)
-//                            }
-//                        }
-//                    }
-//                    .accentColor(Color("MainColor"))
-//                }
-//            }
+            GeometryReader { geometry in
+                if isLoading {
+                    LaunchScreenView()
+                        .edgesIgnoringSafeArea(.all)
+                        .onAppear {
+                            let dispatchGroup = DispatchGroup()
+                            DispatchQueue.global().async(group: dispatchGroup) {
+                                dispatchGroup.enter()
+                                firebaseSM.getCafes(dispatchGroup: dispatchGroup)
+                            }
+                            dispatchGroup.notify(queue: DispatchQueue.main) {
+                                isLoading.toggle()
+                            }
+                        }
+                } else {
+                    NavigationView {
+                        if isFirst {
+                            IntroView()
+                        } else {
+                            VStack(spacing: 0) {
+                                switch viewRouter.currentPage {
+                                case .home:
+                                    MainView(firebaseSM: firebaseSM, ifFirst: true)
+                                case .bookmarked:
+                                    MyBookmarkView(firebaseSM: firebaseSM)
+                                }
+                                Spacer(minLength: 0)
+                                ZStack {
+                                    HStack {
+                                        // 홈
+                                        TabBarIcon(viewRouter: viewRouter, assignedPage: .home, width: geometry.size.width/3, height: geometry.size.height/28, iconName: "home")
+
+                                        // AR
+                                        NavigationLink(destination: ARMainView()) {
+                                            VStack {
+                                                Image("ar")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(width: geometry.size.width/3, height: geometry.size.height/28)
+                                                    .foregroundColor(.gray)
+                                            }
+                                        }
+
+                                        // 북마크
+                                        TabBarIcon(viewRouter: viewRouter, assignedPage: .bookmarked, width: geometry.size.width/3, height: geometry.size.height/28, iconName: "bookmarks")
+                                    }
+                                    .frame(width: geometry.size.width, height: geometry.size.height/14)
+                                    .overlay(Divider(), alignment: .top)
+                                }
+                                .padding(0)
+                            }
+                        }
+                    }
+                    .accentColor(Color("MainColor"))
+                }
+            }
         }
     }
 }
